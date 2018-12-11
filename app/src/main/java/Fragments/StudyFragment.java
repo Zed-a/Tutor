e07714eea739cb4d61b23381f5b058471dae30de
nan <1289704070@qq.com>
2018-12-11 17:18:07 +0800

    update CityPicker

--- app/src/main/java/Fragments/StudyFragment.java
+++ app/src/main/java/Fragments/StudyFragment.java
@@ -2,17 +2,30 @@ package Fragments;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nan.tutor.R;
+import com.zaaach.citypicker.CityPicker;
+import com.zaaach.citypicker.adapter.OnPickListener;
+import com.zaaach.citypicker.model.City;
+import com.zaaach.citypicker.model.HotCity;
+import com.zaaach.citypicker.model.LocatedCity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StudyFragment extends Fragment {
+public class StudyFragment extends Fragment implements View.OnClickListener{
+
+    private TextView city;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,4 +34,61 @@ public class StudyFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_study, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        initView();
+
+    }
+
+    private void initView(){
+
+        city = getView().findViewById(R.id.city);
+        city.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.city:
+                List<HotCity> hotCities = new ArrayList<>();
+                hotCities.add(new HotCity("北京", "北京", "101010100")); //code为城市代码
+                hotCities.add(new HotCity("上海", "上海", "101020100"));
+                hotCities.add(new HotCity("广州", "广东", "101280101"));
+                hotCities.add(new HotCity("深圳", "广东", "101280601"));
+                hotCities.add(new HotCity("杭州", "浙江", "101210101"));
+
+                CityPicker.from(this) //activity或者fragment
+                        .enableAnimation(true)	//启用动画效果，默认无
+//                .setAnimationStyle(anim)	//自定义动画
+                        .setLocatedCity(new LocatedCity("杭州", "浙江", "101210101"))
+                        .setHotCities(hotCities)	//指定热门城市
+                        .setOnPickListener(new OnPickListener() {
+                            @Override
+                            public void onPick(int position, City data) {
+                                Toast.makeText(getActivity(), data.getName(), Toast.LENGTH_SHORT).show();
+                                city.setText(data.getName());
+                            }
+
+                            @Override
+                            public void onCancel(){
+                                Toast.makeText(getActivity(), "取消选择", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onLocate() {
+                                //定位接口，需要APP自身实现，这里模拟一下定位
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        //定位完成之后更新数据
+//                                CityPicker.getInstance()
+//                                        .locateComplete(new LocatedCity("深圳", "广东", "101280601"), LocateState.SUCCESS);
+                                    }
+                                }, 3000);
+                            }
+                        })
+                        .show();
+        }
+    }
 }
